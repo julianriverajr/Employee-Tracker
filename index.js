25dfddabe44e0b88eeeee1636d401ec01a0faaa8
Julian Rivera Jr <julianriverajr@gmail.com>
2020-10-27 22:45:18 -0500

    set prompt to variable to grab answer, made func for questions if wanting to add a new dept, started for role

--- index.js
+++ index.js
@@ -18,19 +18,60 @@ connection.connect(function (err) {
     console.log("connected as id " + connection.threadId);
 });
 
-function promptUser() {
-    return inquirer.prompt([
+async function promptUser() {
+    let questions = await inquirer.prompt([
         {
             type: "list",
             message: "What would you like to do?",
             choices: ['add a department', 'add a role', 'add an employee', 'view departments', 'view roles', 'view all employees', 'update employee roles'],
             name: "initialChoice"
         }
-    ])
+    ]);
+    console.log("You've chosen " + questions.initialChoice);
+    if (questions.initialChoice === 'add a department'){
+        deptQs();
+    }
+    else if (questions.initialChoice === 'add a role'){
+        roleQs();
+    }
+    else if (questions.initialChoice === 'add an employee'){
+        employeeQs();
+    }
+    else if (questions.initialChoice === 'view departments'){
+        viewDepts();
+    }
+    else if (questions.initialChoice === 'view roles'){
+        viewRoles();
+    }
+    else if (questions.initialChoice === 'view all employees'){
+        viewEmployees();
+    }
+    else if (questions.initialChoice === 'update employee roles'){
+        updateEmployeeRoles();
+    }
+    else return;
 };
-function getAnswers(answers){
-    return `${answers.initialChoice}`
-    
+
+async function deptQs(){
+    let deptQuestions = await inquirer.prompt([
+        {
+            type: "input",
+            message: "What is this new department's name?",
+            name: "newDeptName"
+        }
+    ]);
+    console.log("You've named the new department " + deptQuestions.newDeptName);
+};
+
+async function roleQs(){
+    let roleQuestions = await inquirer.prompt([
+        {
+            type: "input",
+            message: "What is the name of this new role?",
+            name: "newRoleName"
+        }
+    ]);
+    console.log("You've named the new role " + roleQuestions.newRoleName);
 }
-getAnswers();
+
 promptUser();
