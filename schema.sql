721e99a2bbae6c0327aca08de081a5a38c51597d
Julian Rivera Jr <julianriverajr@gmail.com>
2020-10-27 19:49:11 -0500

    added roles, and employees table to schema

--- schema.sql
+++ schema.sql
@@ -3,7 +3,21 @@ CREATE DATABASE employee_tracker_db;
 USE employee_tracker_db;
 
 CREATE TABLE departments (
-    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    id INT NOT NULL PRIMARY KEY,
     name VARCHAR(30) NOT NULL
-);
+)
 
+CREATE TABLE roles (
+    id INT NOT NULL PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(9,4) NOT NULL,
+    department_id INT NOT NULL
+)
+
+CREATE TABLE employees (
+    id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+    first_name VARCHAR(30),
+    last_name VARCHAR(30),
+    role_id INT NOT NULL,
+    manager_id INT
+)
